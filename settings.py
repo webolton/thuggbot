d21edc3b809fceabbf3b092aa3f86cf375658bb2
William Bolton <william.ellet@gmail.com>
2020-12-27 13:57:46 -0500

    Add dot env for global configuration variables

---
+++ settings.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+THUGGBOT_ENV = os.getenv('THUGGBOT_ENV') # set production or leave blank
+
+load_dotenv(verbose=True)
+
+if THUGGBOT_ENV == 'production':
+  env_path = Path('.') / '.env.production'
+else:
+  env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
--- thuggs/__main__.py
+++ thuggs/__main__.py
@@ -1,5 +1,7 @@
 from .app import Thuggs
 import sys
+import os
+os.environ["SETTINGS_MODULE"] = 'settings'
 
 # get commandline arguments to detect either cats or humans
 identification_type = sys.argv[1]
